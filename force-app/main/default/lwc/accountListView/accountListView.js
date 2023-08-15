c76eef89f1a77fac6e3e72036d16bfbca3e9418d
Baji syed <baji340@gmail.com>
2023-08-15 20:01:47 +0530

    updatedlogic

--- force-app/main/default/lwc/accountListView/accountListView.js
+++ force-app/main/default/lwc/accountListView/accountListView.js
@@ -1,11 +1,5 @@
 import { LightningElement,track, wire } from 'lwc';
 import fetchAccountRecord from '@salesforce/apex/accountListView.fetchAccountRecord';
-const columns = [
-    { label: 'Id', fieldName: 'Id' },
-    { label: 'Name', fieldName: 'Name' },
-    { label: 'Type', fieldName: 'Type' },
-    { label: 'Website', fieldName: 'Website', type: 'url' }
-];
 export default class AccountListView extends LightningElement {
     @track rowOffset = 0;
     @track tableLoadingState = false;
@@ -14,11 +8,42 @@ export default class AccountListView extends LightningElement {
     @track offset=0;
     @track Prevoffset=0;
     limit = 7;
-
+    @track columns = [{
+        label: 'Account name',
+        fieldName: 'Name',
+        type: 'text',
+        sortable: true
+    },
+    {
+        label: 'Type',
+        fieldName: 'Type',
+        type: 'text',
+        sortable: true
+    },
+    {
+        label: 'Phone',
+        fieldName: 'Phone',
+        type: 'phone',
+        sortable: true
+    },
+    {
+        label: 'Website',
+        fieldName: 'Website',
+        type: 'url',
+        sortable: true
+    },
+    {
+        label: 'Rating',
+        fieldName: 'Rating',
+        type: 'test',
+        sortable: true
+    }
+];
     @wire(fetchAccountRecord,{offset: '$offset', l : '$limit' }) wiredAccounts({ error, data }) {
         this.tableLoadingState = false;
         if(data){
             this.accounts=data;
+            console.log('Data'+JSON.stringify(this.accounts));
             this.err=undefined;
             if(this.accounts.length==0)
                 this.offset=this.Prevoffset;
@@ -27,7 +52,7 @@ export default class AccountListView extends LightningElement {
             this.accounts=undefined;
         }
     }
-    handlePrev (_event) {
+    handlePrev (event) {
         //window.clearTimeout(this.delayTimeout);        
         if(this.offset - this.limit >=0)
         {
@@ -37,7 +62,7 @@ export default class AccountListView extends LightningElement {
         }
     }
 
-    handleNext (_event) {
+    handleNext (event) {
         //window.clearTimeout(this.delayTimeout);
         this.tableLoadingState = true;
         this.Prevoffset=this.offset;
